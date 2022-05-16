159a442d5c4f9c5f4e0aeaf063fdf7d20ac71a0c
Vittor Faria Pereira <vittorfpereira@gmail.com>
2022-05-16 12:16:07 -0300

    feature (partial): renaming logic.

--- gryphon/core/init_from_existing.py
+++ gryphon/core/init_from_existing.py
@@ -1,12 +1,15 @@
 import json
+import logging
 import os
 import shutil
 from pathlib import Path
 
 from ..constants import CONDA, VENV, REQUIREMENTS
-from ..constants import VENV_FOLDER, CONDA_FOLDER, GRYPHON_RC, YES, NO
+from ..constants import VENV_FOLDER, CONDA_FOLDER, GRYPHON_RC, YES
 from ..core.operations import EnvironmentManagerOperations, RCManager
 from ..core.settings import SettingsManager
+
+logger = logging.getLogger('gryphon')
 
 
 def check_for_history(folder: Path):
@@ -60,17 +63,42 @@ def create_environment(path: Path, env_manager=None):
         return EnvironmentManagerOperations.create_venv(path)
 
 
+def rename_dir(path):
+    i = 0
+    root_path = path
+    while path.is_dir():
+        i += 1
+        path = f"{root_path}_{i}"
+
+    shutil.copytree(
+        src=root_path,
+        dst=path
+    )
+    shutil.rmtree(root_path)
+
+    return path
+
+
 def process_environment_core(location, env_manager, use_existing_environment, env_path, history_file):
+
     if use_existing_environment == "no_delete":
         shutil.rmtree(env_path)
 
     if use_existing_environment != YES:
-        if env_path.is_dir():
-            pass
-            # rename folder
-        env_path = create_environment(location, env_manager=env_manager)
+
+        if env_manager == CONDA and (location / "envs").is_dir():
+            new_path = rename_dir(location / CONDA_FOLDER)
+            env_path = create_environment(location, env_manager=env_manager)
+            logger.warning(f"Renaming existing environment {env_path} to {new_path}")
+
+        elif env_manager == VENV and (location / VENV_FOLDER).is_dir():
+            new_path = rename_dir(location / VENV_FOLDER)
+            env_path = create_environment(location, env_manager=env_manager)
+            logger.warning(f"Renaming existing environment {env_path} to {new_path}")
 
     # TODO: put information about the env manager and env into the rc file
+    # RCManager.set_environment_manager(env_manager)
+    # RCManager.set_environment_manager_path(env_path)
 
 
 def init_from_existing(location, env_manager, use_existing_environment, env_path):
