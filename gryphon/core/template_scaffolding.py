d27dfa098b4c6b7ca9ca69322df6f3bd267d40f3
Vittor Faria Pereira <vittorfpereira@gmail.com>
2022-06-20 11:19:59 -0300

    feature: Installing hooks at new project start and into template scaffold rendering.

--- gryphon/core/template_scaffolding.py
+++ gryphon/core/template_scaffolding.py
@@ -4,12 +4,13 @@ from pathlib import Path
 from .common_operations import (
     init_new_git_repo, initial_git_commit,
 )
-from .operations import EnvironmentManagerOperations
-from .operations.bash_utils import BashUtils
-from .operations.settings import SettingsManager
+from .operations import (
+    EnvironmentManagerOperations, BashUtils,
+    SettingsManager, PreCommitManager
+)
 from ..constants import (
-    DATA_PATH, SUCCESS,
-    INIT, CONDA, VENV, VENV_FOLDER
+    DATA_PATH, SUCCESS, REQUIREMENTS,
+    INIT, CONDA, VENV, VENV_FOLDER, CONDA_FOLDER
 )
 
 logger = logging.getLogger('gryphon')
@@ -31,8 +32,11 @@ def template_scaffolding(location: Path):
         template_destiny=location,
         template_source=Path(template_path)
     )
-    # TODO: JOIN ALL THE requirements.txt files in one at the time of project init with more than one zip file
-    #  downloaded.
+
+    PreCommitManager.fill_configurations(location)
+
+    # TODO: JOIN ALL THE requirements.txt files in one at the time of project
+    #  init with more than one zip file downloaded.
 
     # Git
     repo = init_new_git_repo(folder=location)
@@ -40,14 +44,28 @@ def template_scaffolding(location: Path):
 
     # ENV Manager
     if env_type == VENV:            # VENV
-        EnvironmentManagerOperations.create_venv(folder=location / VENV_FOLDER, python_version=python_version)
+        env_folder = location / VENV_FOLDER
+        env_folder = EnvironmentManagerOperations.create_venv(env_folder, python_version=python_version)
+        EnvironmentManagerOperations.install_libraries_venv(
+            environment_path=env_folder,
+            requirements_path=location / REQUIREMENTS
+        )
+
     elif env_type == CONDA:
         # CONDA
-        EnvironmentManagerOperations.create_conda_env(location, python_version=python_version)
-        # install_extra_nbextensions_conda(location)
+        env_folder = location / CONDA_FOLDER
+        env_folder = EnvironmentManagerOperations.create_conda_env(env_folder, python_version=python_version)
+        EnvironmentManagerOperations.install_libraries_conda(
+            environment_path=env_folder,
+            requirements_path=location / REQUIREMENTS
+        )
+
     else:
         raise RuntimeError("Invalid \"environment_management\" option on gryphon_config.json file."
                            f"Should be one of {[INIT, CONDA]} but \"{env_type}\" was given.")
+
+    # install pre-commit hooks
+    PreCommitManager.install_pre_commit(env_folder, location)
 
     SettingsManager.add_local_template(str(Path(location).absolute()))
     logger.info("Added new template into the gryphon registry. You will be able to find it inside gryphon according"
@@ -55,3 +73,8 @@ def template_scaffolding(location: Path):
                 " have to fill the template information inside metadata.json file (providing at least the display "
                 "name and the command).")
     logger.log(SUCCESS, "Installation successful!")
+
+# DONE: install pre-commit hooks on init
+# TODO: add .pre-commit-config.yaml on the template folders
+# TODO: prompt whether the template scaffolding is for init or generate]
+# TODO: way to choose which pre-commit hooks ones want to add
